1b2eb33dc9ccf13e3e6567e0883946ad7f854394
Asjad Ali <asjad.ali@ozoneddigital.com>
2023-01-10 02:13:38 +0500

    user type

--- pages/signup.vue
+++ pages/signup.vue
@@ -8,6 +8,31 @@
             <h3 class="mb-1 pb-2 pb-md-0 mb-md-4 text-center">Registration Form</h3>
             <form>
               <div class="row">
+                <div class="col-md-12 mb-3">
+
+                  <h6 class="mb-2 pb-1">User Type: </h6>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
+                      value="option1" checked />
+                    <label class="form-check-label" for="femaleGender">Admin</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
+                      value="option2" />
+                    <label class="form-check-label" for="maleGender">Coach</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
+                      value="option3" />
+                    <label class="form-check-label" for="otherGender">User</label>
+                  </div>
+
+                </div>
+              </div>
+              <div class="row">
                 <div class="col-md-6 mb-1">
                   <div class="form-outline">
                     <input type="text" id="firstName" class="form-control form-control-lg" />
